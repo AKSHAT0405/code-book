2a34fdab03564c0c40c34c8a5df8e204a5eb6a3b
AKSHAT0405 <sodaniakshat6@gmail.com>
2026-08-01 15:07:47 +0530

    update in bruteforce sol.

--- arrays/rearrange_by_sign.cpp
+++ arrays/rearrange_by_sign.cpp
@@ -8,22 +8,19 @@ public:
         int n = nums.size();
         vector<int> pos;
         vector<int> neg;
-        vector<int> res(n,0);
-        int pos_index = 0;
-        int neg_index = 1;
         for(int i = 0; i < n; i++){
             if(nums[i] > 0){
                 pos.push_back(nums[i]);
-                res[pos_index] = nums[i];
-                pos_index += 2;
             }
             else{
                 neg.push_back(nums[i]);
-                res[neg_index] = nums[i];
-                neg_index += 2;
             }
         }
-        return res;
+        for(int j = 0; j < n/2; j++){
+            nums[j*2] = pos[j];
+            nums[j*2+1] = neg[j];
+        }
+        return nums;
     }
     vector<int> optimal(vector<int>& nums){
         int n = nums.size();
