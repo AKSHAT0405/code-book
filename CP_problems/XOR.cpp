a85dd1799c62645db8d456ce7588e4045ab2b162
AKSHAT0405 <sodaniakshat6@gmail.com>
2026-08-07 00:41:09 +0530

    update to accepted XOR solution

--- CP_problems/XOR.cpp
+++ CP_problems/XOR.cpp
@@ -3,14 +3,20 @@ using namespace std;
 
 void solve(vector<int>& nums)
 {
-    int ans = 0;
-
-    for (int i = 0; i < nums.size(); i += 2)
-    {
-        ans = ans^nums[i];
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        int x = nums[i];
+        int xorr = 0;
+        for(int j = 0; j<n; j++){
+            if(j != i){
+                xorr = xorr^nums[j];
+            }
+        }
+        if(xorr == x){
+            cout<<x<<endl;
+            break;
+        }
     }
-
-    cout << ans << endl;
 }
 
 int main()
@@ -29,9 +35,10 @@ int main()
         {
             cin >> nums[i];
         }
-
         solve(nums);
     }
 
     return 0;
 }
+
+// codeforces problem - 1698A
