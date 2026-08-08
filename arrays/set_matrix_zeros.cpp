7650daa160ad33894a70525ec6eff16853382397
AKSHAT0405 <sodaniakshat6@gmail.com>
2026-08-08 22:18:32 +0530

    Set Matrix Zeroes

---
+++ arrays/set_matrix_zeros.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void brute(vector<vector<int>>& matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    for(int col = 0; col < n; col++){
+                        if(matrix[i][col] != 0){
+                            matrix[i][col] = -1;
+                        }
+                    }
+                    for(int row = 0; row < n; row++){
+                        if(matrix[row][j] != 0){
+                            matrix[row][j] = -1;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == -1){
+                    matrix[i][j]= 0;
+                }
+            }
+        }
+    }
+    void better(vector<vector<int>>& matrix){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row_status(n,0);
+        vector<int> col_status(m,0);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    row_status[i] = 1;
+                    col_status[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(row_status[i] == 1 || col_status[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    Solution obj;
+    // obj.brute(matrix);
+    obj.better(matrix);
+
+    cout << "Matrix after operation:\n";
+    for (const auto& row : matrix)
+    {
+        for (int val : row)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
