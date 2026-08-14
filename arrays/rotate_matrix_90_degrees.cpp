76a882d3c6d65de06c5189083d5b26befb6217f2
AKSHAT0405 <sodaniakshat6@gmail.com>
2026-08-14 18:50:51 +0530

    optimal sol. of rotate matrix by 90

--- arrays/rotate_matrix_90_degrees.cpp
+++ arrays/rotate_matrix_90_degrees.cpp
@@ -22,6 +22,17 @@ public:
             }
         }
     }
+    void optimal(vector<vector<int>>& matrix){
+        int n = matrix.size();
+        for (int i = 0; i < n-1; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
 };
 
 int main()
@@ -56,3 +67,6 @@ int main()
 }
 
 // brute : tc - O(n^2)  , sc - O(n^2)
+// optimal : tc - O(n^2)  , sc - O(1)
+//  opitmal -- first transpose then reverse each array .
+// here the transpose takes O(n/2*n/2) and reverse takes O(n*n/2) so total of O(n^2).
