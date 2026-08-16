b2c29fde34b6dbedea34eded5a94a720595919cf
AKSHAT0405 <sodaniakshat6@gmail.com>
2026-08-16 17:04:25 +0530

    optimal sol. of Count Subarray sum Equals K

--- arrays/Count_Subarray_Sum_Equals_K.cpp
+++ arrays/Count_Subarray_Sum_Equals_K.cpp
@@ -19,16 +19,20 @@ public:
         return count;
     }
 
-    int better(vector<int>& nums, int k)
-    {
-        // Write your better solution here
-        return 0;
-    }
-
     int optimal(vector<int>& nums, int k)
     {
-        // Write your optimal solution here
-        return 0;
+        int n = nums.size();
+        int count = 0;
+        int sum = 0;
+        map<long , long> mpp;
+        mpp[0] = 1;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            int rem = sum - k;
+            count += mpp[rem];
+            mpp[sum] += 1;
+        }
+        return count;
     }
 };
 
@@ -57,3 +61,7 @@ int main()
 
     return 0;
 }
+
+// brute : tc - O(n^2) , sc - O(1)
+// optimal : tc - O(n*logn) , sc - O(n)
+// optimal { use of prefix sum concept and map }
